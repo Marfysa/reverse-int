7896d10cecf67f8528958223f71daca09f3f5271
Maria Kurakevich <marfysa14@gmail.com>
2021-08-10 13:43:36 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+ 
+  if (n < 0) {
+    let arr = String(n).split('').reverse().slice(0,3).join('');
+    return arr;
+  } else {
+      let arr = String(n).split('').reverse().join('');
+      return arr;
+  } 
 }
